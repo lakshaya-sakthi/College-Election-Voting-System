defa42f11884d48de6c0be48ce5a8861b7c5c652
lakshaya-sakthi <lakshyasakthi@gmail.com>
2026-04-06 14:56:34 +0530

    changed port number to dynamic

--- server/server.js
+++ server/server.js
@@ -470,5 +470,10 @@ app.get("/api/results", auth, async (req, res) => {
 // =======================
 // SERVER START
 // =======================
-
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.get("/", (req, res) => {
+  res.send("College Election Voting System API is running 🚀");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
